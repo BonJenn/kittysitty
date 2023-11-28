69c278d6c8157e7239e165979865813662d7b2e5
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2023-11-28 01:57:24 -0800

    Attempted to add Supabase and added additional fields to the home search bar

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-fetch('/api/featured-sitters')
-    .then(response => response.json())
-    .then(sitters => {
-        const sittersDiv = document.getElementById('featured-sitters');
-        sitters.forEach(sitter => {
-            const sitterDiv = document.createElement('div');
-            sitterDiv.className = 'featured-sitter';
-            sitterDiv.innerHTML = `
-                <h2>${sitter.name}</h2>
-                <img src="${sitter.image}" alt="${sitter.name}">
-                <p>${sitter.description}</p>
-            `;
-            sittersDiv.appendChild(sitterDiv);
-        });
-    });
+console.log('JavaScript file is loaded');
+
+import { createClient } from '@supabase/supabase-js'
+
+// The rest of your JavaScript code...
+
+
+const supabaseUrl = 'https://pscatoacrcgdsgweoafg.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzY2F0b2FjcmNnZHNnd2VvYWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA4MTkzNzksImV4cCI6MjAxNjM5NTM3OX0.W5-MQg_EetbdSdWv4MyjwpTvGSnpfrokmPYo1byeIzs'
+const database = createClient(supabaseUrl, supabaseAnonKey)
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent form submission
+
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    console.log('First Name:', fname);
+    console.log('Last Name:', lname);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log(fname,lname,email);
+    console.log(database)
+
+
+}) 
